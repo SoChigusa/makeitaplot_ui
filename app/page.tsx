b01939931a13bb5794c81fa8e7af0388d534b8f3
SoChigusa <so.chigusa.pp@gmail.com>
2024-04-14 15:10:40 -0700

    Connect to api

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,92 @@
 'use client';
 
-import { useState, useRef } from "react";
+import { useState, useRef, MouseEventHandler } from "react";
 import type { PutBlobResult } from "@vercel/blob";
+import { AppBar, Backdrop, Box, Button, CircularProgress, Container, FormControl, Input, Stack, Toolbar, Typography } from "@mui/material";
 
 export default function Home() {
   const inputFileRef = useRef<HTMLInputElement>(null);
   const [blob, setBlob] = useState<PutBlobResult | null>(null);
+  const [imageUrl, setImageUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleClickPlot: MouseEventHandler<HTMLButtonElement> = async (event) => {
+    setLoading(true);
+    event.preventDefault();
+
+    // File check
+    if (!inputFileRef.current?.files) {
+      throw new Error('No file selected');
+    }
+
+    // File upload
+    const file = inputFileRef.current.files[0];
+    const response = await fetch(
+      `/api/upload?filename=${file.name}`,
+      {
+        method: 'POST',
+        body: file,
+      },
+    );
+
+    // Preserve file info
+    const newBlob = (await response.json()) as PutBlobResult;
+    setBlob(newBlob);
+
+    console.log(`https://makeitaplot-api.vercel.app/plot?url=${newBlob.downloadUrl}`);
+
+    // Plot
+    await fetch(`https://makeitaplot-api.vercel.app/plot?url=${newBlob.downloadUrl}`, {
+      method: 'GET'
+    }).then(res => res.blob())
+      .then(obj => {
+        const url = URL.createObjectURL(obj);
+        setImageUrl(url);
+      });
+
+    setLoading(false);
+  };
 
   return (
-    <>
-      <form onSubmit={async (event) => {
-        event.preventDefault();
-
-        if (!inputFileRef.current?.files) {
-          throw new Error('No file selected');
-        }
-
-        const file = inputFileRef.current.files[0];
-        const response = await fetch(
-          `/api/upload?filename=${file.name}`,
-          {
-            method: 'POST',
-            body: file,
-          },
-        );
-
-        const newBlob = (await response.json()) as PutBlobResult;
-        setBlob(newBlob);
-      }}
-      >
-        <input name="file" ref={inputFileRef} type="file" required />
-        <button type="submit">Upload</button>
-      </form>
-      {blob && (
-        <>
-          <h2>{blob?.pathname}</h2>
-          <h2>{blob?.downloadUrl}</h2>
-        </>
-      )}
-    </>
+    <Box>
+      <AppBar position='static' sx={{ marginBottom: 1 }}>
+        <Container maxWidth='lg'>
+          <Toolbar>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              MakeItAPlot!
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container>
+        <Stack spacing={2}>
+          <Box display='flex' justifyContent='center'>
+            <img src={imageUrl} alt='Generated Plot' />
+          </Box>
+          <FormControl>
+            <Stack spacing={2} direction="row" sx={{ marginX: 'auto' }}>
+              <Input
+                name="file"
+                inputRef={inputFileRef}
+                type="file"
+                required
+              />
+              <Button
+                variant="contained"
+                onClick={handleClickPlot}
+              >
+                Plot
+              </Button>
+            </Stack>
+          </FormControl>
+        </Stack>
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loading}
+        >
+          <CircularProgress />
+        </Backdrop>
+      </Container>
+    </Box>
   );
 }
